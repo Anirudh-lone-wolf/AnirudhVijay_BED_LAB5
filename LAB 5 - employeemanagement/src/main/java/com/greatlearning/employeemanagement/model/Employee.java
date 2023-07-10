ced7cc54701e8f5817c38a3159739d6622075460
Anirudh V <66291656+Anirudh-lone-wolf@users.noreply.github.com>
2023-07-10 23:21:16 +0530

    Update Employee.java

--- LAB 5 - employeemanagement/src/main/java/com/greatlearning/employeemanagement/model/Employee.java
+++ LAB 5 - employeemanagement/src/main/java/com/greatlearning/employeemanagement/model/Employee.java
@@ -21,14 +21,6 @@ public class Employee {
 	public Employee() {
 	}
 
-//	public Employee(int id, String firstName, String lastName, String email) {
-//		super();
-//		this.id = id;
-//		this.firstName = firstName;
-//		this.lastName = lastName;
-//		this.email = email;
-//	}
-
 	public Employee(String firstName, String lastName, String email) {
 		super();
 		this.firstName = firstName;
